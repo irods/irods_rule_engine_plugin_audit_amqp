43f2ea950e694fbecc4480a57545f3763121a123
Justin James <jjames@renci.org>
2017-04-07 16:32:48 -0400

    test

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -15,13 +15,13 @@ def install_cmake_and_add_to_front_of_path():
 
 def get_build_prerequisites_all():
     return ['irods-externals-clang3.8-0',
-            'irods-externals-cppzmq4.1-0',
-            'irods-externals-libarchive3.1.2-0',
-            'irods-externals-avro1.7.7-0',
+#            'irods-externals-cppzmq4.1-0',
+#            'irods-externals-libarchive3.1.2-0',
+#            'irods-externals-avro1.7.7-0',
             'irods-externals-clang-runtime3.8-0',
             'irods-externals-boost1.60.0-0',
-            'irods-externals-jansson2.7-0',
-            'irods-externals-zeromq4-14.1.3-0',
+#            'irods-externals-jansson2.7-0',
+#            'irods-externals-zeromq4-14.1.3-0',
             'qpid-with-proton0.34-0']
 
 def get_build_prerequisites_apt():
